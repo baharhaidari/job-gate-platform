a4881bcb930b53a6bce99969fd96f3004325c7a2
Bahar Haidari <149867578+baharhaidari@users.noreply.github.com>
2024-10-30 11:07:33 +0430

    Update Hero.jsx

--- frontend/src/Components/Hero/Hero.jsx
+++ frontend/src/Components/Hero/Hero.jsx
@@ -21,7 +21,7 @@ export default function Hero() {
   const fetchUsers = async () => {
     try {
       const response = await axios.get(
-        "https://job-gate-repo-1-2.onrender.com/api/user/get-users",
+        "https://job-gate-platform.onrender.com/api/user/get-users",
         {
           headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
         }
@@ -35,7 +35,7 @@ export default function Hero() {
   const fetchJobs = async () => {
     try {
       const response = await axios.get(
-        "https://job-gate-repo-1-2.onrender.com/api/user/get-today-jobs",
+        "https://job-gate-platform.onrender.com/api/user/get-today-jobs",
         {
           headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
         }
